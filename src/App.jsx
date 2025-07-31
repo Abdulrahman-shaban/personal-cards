b24a7fea7e9cd684f9c23de5749c37017343fa18
AbdulRahman Mohamed <abdulRahman.mohamed.shaban@gmail.com>
2025-07-31 15:31:25 +0300

    ready to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import AddCard from "./pages/AddCard";
 import EditCard from "./pages/EditCard";
